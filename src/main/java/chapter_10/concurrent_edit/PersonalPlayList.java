794bb6cc1d5d21000593eb18c76614b52cf67112
SeolYoungKim <nasur4da@gmail.com>
2022-12-14 00:36:36 +0900

    chapter10: 부모 클래스에 필드가 추가됨으로써, 자식 클래스의 메서드도 함께 수정해야 하는 문제 발생

--- src/main/java/chapter_10/concurrent_edit/PersonalPlayList.java
+++ src/main/java/chapter_10/concurrent_edit/PersonalPlayList.java
@@ -3,5 +3,6 @@ package chapter_10.concurrent_edit;
 public class PersonalPlayList extends PlayList {
     public void remove(Song song) {
         tracks().remove(song);
+        singers().remove(song.singer()); // 부모 클래스와 함께 수정되어야 한다.
     }
 }
--- src/main/java/chapter_10/concurrent_edit/PlayList.java
+++ src/main/java/chapter_10/concurrent_edit/PlayList.java
@@ -1,16 +1,24 @@
 package chapter_10.concurrent_edit;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PlayList {
     private List<Song> tracks = new ArrayList<>();
+    private Map<String, String> singers = new HashMap<>();
 
     public void append(Song song) {
-        tracks().add(song);
+        tracks.add(song);
+        singers.put(song.singer(), song.title());
     }
 
     public List<Song> tracks() {
         return tracks;
+    }
+
+    public Map<String, String> singers() {
+        return singers;
     }
 }
